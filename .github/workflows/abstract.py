a980b875dcd0aa539608b42c8359be73eb0e991f
nachoGJ <94113655+nachoGJ@users.noreply.github.com>
2021-11-18 11:26:19 +0100

    Update abstract.py

--- .github/workflows/abstract.py
+++ .github/workflows/abstract.py
@@ -10,3 +10,30 @@ for line in infile:
 	url = "http://togows.dbcls.jp/entry/ncbi-pubmed/" + ID + "/"
 	r = requests.get(url, allow_redirects=True)
 	outfile.write(r.content)
+
+# Getting the abstracts in the list
+import random
+infile = open("abstracts.dat", "r")
+flag = False
+Abstracts = list()
+Abs = ""
+        
+
+for line in infile:
+
+    End = re.search(r"^[A-Z]+\s+", line)
+    if End is not None and Abs != "":
+        flag = False
+        Abstracts.append(Abs)        
+        Abs = ""
+
+    if flag: 
+        line = re.sub("\n", " ", line)
+        Abs += line 
+    
+    Reresult = re.search(r"^AB\s+-\s(.+)", line)
+    if Reresult is not None:
+        flag = True
+        Abs += Reresult.group(1).strip()
+
+print(Abstracts)  
