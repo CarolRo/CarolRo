02c8c06ac4022125ca85f972ec2dc62f6f6b3251
nachoGJ <94113655+nachoGJ@users.noreply.github.com>
2021-11-29 10:11:18 +0100

    Update file_downloader.py

--- Programs/file_downloader.py
+++ Programs/file_downloader.py
@@ -6,12 +6,15 @@ try:
     infile = open(IDs_file, "r")
     outfile = open("abstracts.dat", "wb")
     for line in infile:
-	    ID = line
-	    url = "http://togows.dbcls.jp/entry/ncbi-pubmed/" + ID + "/"
-	    r = requests.get(url, allow_redirects=True)
-	    outfile.write(r.content)
+	    ID = re.search(r"^(\d+)", line)
+	    if ID is not None:
+	        url = "http://togows.dbcls.jp/entry/ncbi-pubmed/" + ID.group(1) + "/"
+	        r = requests.get(url, allow_redirects=True)
+	        outfile.write(r.content)
+		
+    infile.close()		
     outfile.close()
     print("Your data was downloaded the file is called: abstracts.dat")
-except OSError:
-    print ("Could not open/read file, check if your file exist:", IDs_file)
+except OSError as error:
+    sys.stderr.write("Could not open/read file, check if your file exists:", IDs_file, "\n")
     sys.exit()
