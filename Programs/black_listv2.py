60ba306a9275ec9db7220824f35730cd460ef1f0
CarolRo <31473961+CarolRo@users.noreply.github.com>
2021-11-29 08:14:00 +0100

    Update black_listv2.py

--- Programs/black_listv2.py
+++ Programs/black_listv2.py
@@ -1,30 +1,40 @@
+#!/usr/bin/env python3
 
-infile = open("abstracts1.dat", "r")
+import re
+import string
+import random
+import math
+
+abs_file= input("Write the name of the file with the abstracts: ")
+infile = open(abs_file, "r")
 flag = False
 Abstracts = list()
 Abs = ""
-        
+
 for line in infile:
     End = re.search(r"^[A-Z]+\s+", line)
     if End is not None and Abs != "":
         flag = False
-        Abstracts.append(Abs)        
+        Abstracts.append(Abs)
         Abs = ""
-    if flag: 
+    if flag:
         line = re.sub("\n", " ", line)
-        Abs += line 
-    
+        Abs += line
+
     Reresult = re.search(r"^AB\s+-\s(.+)", line)
     if Reresult is not None:
         flag = True
         Abs += Reresult.group(1).strip()
 infile.close()
+
 outfile = open("cleanAbs.dat", "w")
 for ABS in Abstracts:
     outfile.write(ABS + "\n")
 outfile.close()
-n = int(len(Abstracts)/10)
+print(len(Abstracts))
+n = int(len(Abstracts) / 10)
 tenperc_abs = random.sample(Abstracts, n)
+#print(len(tenperc_abs))
 wcount = dict()
 for Abstract in tenperc_abs:
     words = [word.strip(string.punctuation).lower() for word in Abstract.split()]
@@ -36,10 +46,13 @@ for Abstract in tenperc_abs:
                 wcount[i] = 0
             if i in wcount:
                 wcount[i] += 1
+#print(sum(wcount.values()))
+for w in sorted(wcount, key=wcount.get, reverse=True):
+    print(w, wcount[w])
 outfile = open("blacklist.dat", "w")
 for w in wcount:
-    n = (wcount[w] / sum(wcount.values())) * 100 
-    if n > 0.009: # Modify this limit when we get the actual range with all the abstracts
+    n = (wcount[w] / sum(wcount.values())) * 100
+    if n > 0.009:  # Modify this limit when we get the actual range with all the abstracts
         outfile.write(w + "\n")
     else:
         pass
