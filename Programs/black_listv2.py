c2b3f87a5cde2e2788a8b051c86cf429c6752adb
nachoGJ <94113655+nachoGJ@users.noreply.github.com>
2021-11-29 11:15:58 +0100

    Update black_listv2.py

--- Programs/black_listv2.py
+++ Programs/black_listv2.py
@@ -1,59 +1,86 @@
-#!/usr/bin/env python3
 
 import re
 import string
 import random
 import math
+import sys
 
-abs_file= input("Write the name of the file with the abstracts: ")
-infile = open(abs_file, "r")
-flag = False
-Abstracts = list()
-Abs = ""
 
-for line in infile:
-    End = re.search(r"^[A-Z]+\s+", line)
-    if End is not None and Abs != "":
-        flag = False
-        Abstracts.append(Abs)
-        Abs = ""
-    if flag:
-        line = re.sub("\n", " ", line)
-        Abs += line
 
-    Reresult = re.search(r"^AB\s+-\s(.+)", line)
-    if Reresult is not None:
-        flag = True
-        Abs += Reresult.group(1).strip()
-infile.close()
+if len(sys.argv) == 1:      # No commandline arguments
+    abs_file = input("Write the name of the file with the downloaded data: ")
+elif len(sys.argv) == 2:    # Something is there
+    abs_file = sys.argv[1]
+else:
+    sys.stderr.write("Usage: blacklist_filter.py <file> \n")
+    sys.exit(1)
 
-outfile = open("cleanAbs.dat", "w")
-for ABS in Abstracts:
-    outfile.write(ABS + "\n")
-outfile.close()
-print(len(Abstracts))
-n = int(len(Abstracts) / 10)
-tenperc_abs = random.sample(Abstracts, n)
-#print(len(tenperc_abs))
-wcount = dict()
-for Abstract in tenperc_abs:
-    words = [word.strip(string.punctuation).lower() for word in Abstract.split()]
-    for i in words:
-        Reresult = re.search(r"([a-z]+(s)?)", i)
-        if Reresult is not None and len(Reresult.group(1)) > 2:
-            i = Reresult.group(1)
-            if i not in wcount:
-                wcount[i] = 0
-            if i in wcount:
-                wcount[i] += 1
-#print(sum(wcount.values()))
-for w in sorted(wcount, key=wcount.get, reverse=True):
-    print(w, wcount[w])
-outfile = open("blacklist.dat", "w")
-for w in wcount:
-    n = (wcount[w] / sum(wcount.values())) * 100
-    if n > 0.009:  # Modify this limit when we get the actual range with all the abstracts
-        outfile.write(w + "\n")
-    else:
-        pass
-outfile.close()
+
+try:
+	infile = open(abs_file, "r")
+
+	(flag, Abs) = (False, "")
+	Abstracts = list()
+	for line in infile:
+	    End = re.search(r"^[A-Z]+\s+", line)
+	    if End is not None and Abs != "":
+	        flag = False
+	        Abstracts.append(Abs)
+	        Abs = ""
+
+	    if flag:
+	        line = re.sub("\n", " ", line)
+	        Abs += line
+
+	    Reresult = re.search(r"^AB\s+-\s(.+)", line)
+	    if Reresult is not None:
+	        flag = True
+	        Abs += Reresult.group(1).strip()
+	infile.close()
+
+	outfile = open("Abstracts.dat", "w")
+	for ABS in Abstracts:
+	    outfile.write(ABS + "\n")
+
+
+	outfile.close()
+except IOError as error:
+    sys.stderr.write("Could not open/read the file due to: " + str(error) + "\n")
+    sys.exit()	
+
+
+try:
+	n = int(len(Abstracts) / 10)
+	sample_abs = random.sample(Abstracts, n)
+
+	wcount = dict()
+	if sample_abs == []:
+		raise Exception ("Data set is not big enough to efficiently use the program.")
+		
+
+	for Abstract in sample_abs:
+		words = [word.strip(string.punctuation).lower() for word in Abstract.split()]
+		for w in words:
+
+			Reresult = re.search(r"([a-z]+(s)?)", w)
+			if Reresult is not None and len(Reresult.group(1)) > 2:
+				w = Reresult.group(1)
+				if w not in wcount:
+					wcount[w] = 0
+				if w in wcount:
+					wcount[w] += 1
+
+	print(len(sample_abs))
+	outfile = open("blacklist.dat", "w")
+	for w in wcount:
+	    n = (wcount[w] / sum(wcount.values())) * 100
+	    print(n)
+	    if len(sample_abs) > 500 and n > 0.009:  
+	        outfile.write(w + "\n")
+	    elif len(sample_abs) < 500 and n > 0.04:
+	    	outfile.write(w + "\n")
+	    else:
+	        pass
+	outfile.close()
+except Exception as error:
+	print("The exception is:", str(error))
